7979cd54d0c08812a1bec2709a0e1801d2e474c7
Rafael Teixeira <rafael@impactaweb.com.br>
2019-11-05 23:00:57 -0300

    Correção do template de alerta

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -1,89 +1,29 @@
 @if ($errors->any())
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <div class="row align-items-center">
-        <div class="col-auto">
-            <span class="icon icon-shape bg-white text-danger rounded-circle shadow h1 mb-0">
-                <i class="fas fa-times"></i>
-            </span>
-        </div>
-        <div class="col">
-            @foreach ($errors->all() as $error)
-                <div class="h3 mb-0 text-white">{{ $error }}</div>
-            @endforeach
-        </div>
-    </div>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
+<div class="alert alert-danger">
+    @foreach ($errors->all() as $error)
+        <div>{{ $error }}</div>
+    @endforeach
 </div>
 
 @elseif (!empty($messages))
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <div class="row align-items-center">
-        <div class="col-auto">
-            <span class="icon icon-shape bg-white text-warning rounded-circle shadow h1 mb-0">
-                <i class="fas fa-exclamation"></i>
-            </span>
-        </div>
-        <div class="col">
-        @foreach ($messages as $message)
-            <div class="h3 mb-0 text-white">{{ $message }}</div>
-        @endforeach
-        </div>
-    </div>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
+<div class="alert alert-warning">
+    @foreach ($messages as $message)
+        <div>{{ $message }}</div>
+    @endforeach
 </div>
 
 @elseif (Session::has('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    <div class="row align-items-center">
-        <div class="col-auto">
-            <span class="icon icon-shape bg-white text-success rounded-circle shadow h1 mb-0"  data-toggle="tooltip" data-original-title="Editar">
-                <i class="fas fa-check"></i>
-            </span>
-        </div>
-        <div class="col">
-            <div class="h3 mb-0 text-white">{!! Session::get('success') !!}</div>
-        </div>
-    </div>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
+<div class="alert alert-success" role="alert">
+    {!! Session::get('success') !!}
 </div>
 
 @elseif (Session::has('info'))
-<div class="alert alert-info alert-dismissible fade show" role="alert">
-    <div class="row align-items-center">
-        <div class="col-auto">
-            <span class="icon icon-shape bg-white text-info rounded-circle shadow h1 mb-0">
-                <i class="fas fa-bullhorn"></i>
-            </span>
-        </div>
-        <div class="col">
-            <div class="h3 mb-0 text-white">{!! Session::get('info') !!}</div>
-        </div>
-    </div>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
+<div class="alert alert-info">
+    {!! Session::get('info') !!}
 </div>
 
 @elseif (Session::has('error'))
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <div class="row align-items-center">
-        <div class="col-auto">
-            <span class="icon icon-shape bg-white text-danger rounded-circle shadow h1 mb-0">
-                <i class="fas fa-times"></i>
-            </span>
-        </div>
-        <div class="col">
-            <div class="h3 mb-0 text-white">{!! Session::get('error') !!}</div>
-        </div>
-    </div>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
+<div class="alert alert-danger">
+    {!! Session::get('error') !!}
 </div>
 @endif
